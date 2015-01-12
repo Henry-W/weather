0c35104ae46497571198bbd4e35b10d8f2b2cf59
Henry-W <henry.white@capgemini.com>
2015-01-12 16:53:59 +0000

    Added javascript and css functionality

---
+++ weather.js
@@ -0,0 +1,13 @@
+/**
+ * Created by HWHITE on 12/01/2015.
+ */
+$(document).ready(function () {
+    $('input[type="radio"]').click(function () {
+        if ($(this).attr('id') == 'edit-weather-date-2') {
+            $('.form-item-weather-date-custom').show();
+        }
+        else {
+            $('.form-item-weather-date-custom').hide();
+        }
+    });
+});
